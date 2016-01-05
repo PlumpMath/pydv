779cbe6801133e42701b970b634d1668c3a443fa
wehu <huwei04@hotmail.com>
2016-01-05 17:51:32 +0800

    update

--- test/test2.py
+++ test/test2.py
@@ -1,1 +1,27 @@
-print('aaaaaaaaa')
+@namespace()
+def bbb(self):
+
+    @entity(self)
+    def foo(self):
+        @action(self)
+        def build(self):
+            yield from cmd("echo foo")
+
+    @entity(self)
+    def bar(self):
+        self.need(self.parent.foo)
+        @action(self)
+        def build(self):
+            yield from cmd("echo bar")
+
+
+    @suite(self)
+    def zoo(self):
+
+        @test(self)
+        def t0(self):
+            self.need(self.parent.parent.bar)
+            @action(self)
+            def run(self):
+                yield from self.build_needs()
+                yield from cmd("echo t0")
